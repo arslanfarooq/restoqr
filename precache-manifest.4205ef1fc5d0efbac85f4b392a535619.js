d2c2efcfab9e017edeba502d451b4f57def08c0e
Seyed Farzam Mirmoeini <farzam.mirmoeini@gmail.com>
2020-08-26 03:21:42 +0430

    Updates

---
+++ precache-manifest.4205ef1fc5d0efbac85f4b392a535619.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1c5c261e6bde7f2531e8699432a31e2e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "41e12607ab6b3432a109",
+    "url": "/static/css/main.59fe9c58.chunk.css"
+  },
+  {
+    "revision": "1f998165a71fa32e07ee",
+    "url": "/static/js/2.ca13c592.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.ca13c592.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "41e12607ab6b3432a109",
+    "url": "/static/js/main.504cb3b1.chunk.js"
+  },
+  {
+    "revision": "0eb5ddd1c60cd6f96a86",
+    "url": "/static/js/runtime-main.a50520d2.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
