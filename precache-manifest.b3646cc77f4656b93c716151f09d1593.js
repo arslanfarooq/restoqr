d79fc2da956e74d028966b4690f65d23a662d1dc
Seyed Farzam Mirmoeini <farzam.mirmoeini@gmail.com>
2020-08-26 15:28:07 +0430

    Updates

---
+++ precache-manifest.b3646cc77f4656b93c716151f09d1593.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8e49af80b48c4fdd80acd816cfbca8f8",
+    "url": "/restoqr/index.html"
+  },
+  {
+    "revision": "f4bff08df3dfcb219d7c",
+    "url": "/restoqr/static/css/main.59fe9c58.chunk.css"
+  },
+  {
+    "revision": "1f998165a71fa32e07ee",
+    "url": "/restoqr/static/js/2.ca13c592.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/restoqr/static/js/2.ca13c592.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f4bff08df3dfcb219d7c",
+    "url": "/restoqr/static/js/main.3c626e8b.chunk.js"
+  },
+  {
+    "revision": "439481f0c04b13a35de4",
+    "url": "/restoqr/static/js/runtime-main.36655161.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/restoqr/static/media/logo.5d5d9eef.svg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.4205ef1fc5d0efbac85f4b392a535619.js"
+  "/restoqr/precache-manifest.b3646cc77f4656b93c716151f09d1593.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/restoqr/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
